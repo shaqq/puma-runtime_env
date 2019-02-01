59248784c52f3070af6791ae168a56efeb1a60dc
Shaker Islam <shakerislam@gmail.com>
2019-02-01 15:43:10 -0600

    initial commit

---
+++ lib/puma/plugin/runtime_env.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "puma/plugin"
+
+# Graciously, from https://github.com/rails/rails/blob/94b5cd3a20edadd6f6b8cf0bdf1a4d4919df86cb/activesupport/lib/active_support/inflector/methods.rb#L69
+def camelize(term, uppercase_first_letter = true)
+  string = term.to_s
+  if uppercase_first_letter
+    string = string.sub(/^[a-z\d]*/) { |match| inflections.acronyms[match] || match.capitalize }
+  else
+    string = string.sub(inflections.acronyms_camelize_regex) { |match| match.downcase }
+  end
+  string.gsub!(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{inflections.acronyms[$2] || $2.capitalize}" }
+  string.gsub!("/".freeze, "::".freeze)
+  string
+end
+
+Puma::Plugin.create do
+  def config(c)
+    adapter_name = ENV["PUMA_RUNTIME_ENV_ADAPTER"] || "k8s"
+
+    begin
+      require "puma/runtime_env/#{adapter_name}"
+    rescue LoadError => e
+      puts "Could not find an adapter for puma-runtime_env"
+      puts e
+      return
+    end
+
+    adapter = "Puma::RuntimeEnv::#{camelize(adapter_name)}".new
+    poll_interval = Integer(ENV["PUMA_RUNTIME_ENV_INTERVAL"]) rescue 5
+
+    restricted_envs = ["PUMA_RUNTIME_ENV"]
+    other_restricted_envs = ENV.fetch("PUMA_RUNTIME_ENV_RESTRICTED", [])
+    restricted_envs.push(*other_restricted_envs)
+
+    if workers_supported?
+      c.on_worker_boot do
+        Thread.new do
+
+          loop do
+            sleep(poll_interval)
+
+            new_envs = adapter.get_env
+
+            new_envs.each do |key, value|
+              next if key.start_with? *restricted_envs
+
+              ENV[key] = value
+            end
+          end
+
+        end
+      end
+    end
+
+  end
+end
+
---
+++ puma-runtime_env.gemspec
@@ -0,0 +1,24 @@
+
+lib = File.expand_path("../lib", __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = "puma-runtime_env"
+  spec.version       = "0.1"
+  spec.authors       = ["shaker"]
+  spec.email         = ["shakerislam@gmail.com"]
+
+  spec.summary       = %q{Write a short summary, because RubyGems requires one.}
+  spec.homepage      = "https://github.com/shaqq/puma-runtime_env"
+  spec.license       = "Apache"
+
+  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
+    f.match(%r{^(test|spec|features)/})
+  end
+  spec.bindir        = "exe"
+  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.16"
+  spec.add_development_dependency "rake", "~> 10.0"
+end
